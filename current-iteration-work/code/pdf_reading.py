dbd08b5acd234438e5ef5b0e80f0842b05c76db5
ottojonas <ottobjonas@icloud.com>
2024-12-05 10:21:22 +0000

    text to speech is working and file reading is working

--- current-iteration-work/code/pdf_reading.py
+++ current-iteration-work/code/pdf_reading.py
@@ -1,5 +1,5 @@
-import json
 import os
+import re
 
 from dotenv import load_dotenv
 from icecream import ic
@@ -11,10 +11,12 @@ class PDFReading:
 
     def __init__(self):
         self.api_key = self.load_environment()
+        ic(self.api_key)
         self.openai_client = OpenAI(api_key=self.api_key)
 
     def load_environment(self):
         load_dotenv()
+        ic(os.getenv("OPENAI_API_KEY"))
         return os.getenv("OPENAI_API_KEY")
 
     def extract_text_from_pdf(self, pdf_path, output_path):
@@ -23,7 +25,9 @@ class PDFReading:
         for page in reader.pages:
             text += page.extract_text()
 
-        with open(output_path, "w", encoding="utf-8") as file:
+        text = re.sub(f"[^a-zA-Z0-9\s\.,;:!?'\@-]", "", text)
+
+        with open(output_path, "w", encoding="latin-1") as file:
             file.write(text)
 
         ic(f"{output_path} file saved successfully")
--- current-iteration-work/code/text_to_speech.py
+++ current-iteration-work/code/text_to_speech.py
@@ -1,17 +1,45 @@
-import json
 import os
 
+import openai
 from dotenv import load_dotenv
 from icecream import ic
-from openai import OpenAI
 
 
 class TextToSpeech:
 
     def __init__(self):
         self.api_key = self.load_environment()
-        self.openai_client = OpenAI(api_key=self.api_key)
+        openai.api_key = self.api_key
 
     def load_environment(self):
         load_dotenv()
         return os.getenv("OPENAI_API_KEY")
+
+    def transcribe_text_to_speech(self, input_file, output_file):
+        try:
+            with open(input_file, "r", encoding="latin-1") as file:
+                text = file.read()
+
+            response = openai.audio.speech.create(
+                model="tts-1",
+                voice="nova",
+                input=text,
+            )
+
+            audio_content = response.content
+
+            with open(output_file, "wb") as audio_file:
+                audio_file.write(audio_content)
+
+            ic(f"audio content written to {output_file}")
+        except FileNotFoundError:
+            ic(f"error: the file: {input_file} was not found")
+        except Exception as e:
+            ic(f"an error occured: {e}")
+
+
+tts = TextToSpeech()
+tts.transcribe_text_to_speech(
+    "current-iteration-work/extracted-files/extracted_text_pdf.txt",
+    "current-iteration-work/extracted-files/text_to_speech.mp3",
+)
