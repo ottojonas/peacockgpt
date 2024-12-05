a16f224aabac44f3bbf6f4af973c9265362ede17
ottojonas <ottobjonas@icloud.com>
2024-12-05 13:48:01 +0000

    collated all text from pdf files to txt file | other random stuff

--- current-iteration-work/code/main.py
+++ current-iteration-work/code/main.py
@@ -2,7 +2,7 @@ import os
 
 from assistant import Assistant
 from dotenv import load_dotenv
-from icecream import ic  # noqa: F401
+from icecream import ic
 from openai import OpenAI
 from pdf_reading import PDFReading
 from text_to_speech import TextToSpeech
@@ -13,7 +13,7 @@ class Main:
     def __init__(self):
         self.api_key = self.load_environment()
         self.openai_client = OpenAI(api_key=self.api_key)
-        self.assistant = Assistant(api_key=self.api_key)
+        self.assistant = Assistant()
         self.pdf_reading = PDFReading()
         self.text_to_speech = TextToSpeech()
 
@@ -21,6 +21,31 @@ class Main:
         load_dotenv()
         return os.getenv("OPENAI_API_KEY")
 
+    def read_pdf(self, file_path: str, output_path: str):
+        text = self.pdf_reading.extract_text_from_pdf(file_path, output_path)
+        return text
+
+    def generate_response(self, prompt):
+        response = self.assistant.generate_response(prompt)
+        return response
+
+    def speak_text(self, text):
+        self.text_to_speech(text)
+
+    def run_chatbot(self):
+        pdf_text = self.read_pdf(
+            "current-iteration-work/training-documents/how-to-open-a-new-account-nav.pdf",
+            "current-iteration-work/extracted-files/extracted_text_pdf.txt",
+        )
+        ic(f"pdf_text: {pdf_text}")
+
+        user_input = input("Enter query: ")
+        response = self.generate_response(user_input)
+        print(f"assistant: {response}")
+
+        self.speak_text(response)
+
 
 if __name__ == "__main__":
-    pass
+    main = Main()
+    main.run_chatbot()
