847d68975d314780796d70165d4cd5b7796ab16b
ottojonas <ottobjonas@icloud.com>
2024-12-13 10:24:19 +0000

    linux stuff :)

--- frontend/build/static/chunks/webpack.js
+++ frontend/build/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "374accb5ef63b027"; }
+/******/ 		__webpack_require__.h = function() { return "b7fe6afb4012ace8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
