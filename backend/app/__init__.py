fc1cc43d583e0d1374be49f2656d404d12fb786b
ottojonas <ottobjonas@outlook.com>
2025-02-10 17:24:09 +0000

    updated routes and __init__ for mongo

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,20 @@
 # * Initialize the Flask/Django app.
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+mongo = PyMongo()
+load_dotenv()
 
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///conversations.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-
+    app.config["MONGO_URI"] = os.getenv("MONGODB_URI")
+    mongo.init_app()
     from .routes import routes
 
     app.register_blueprint(routes, url_prefix="/api")
