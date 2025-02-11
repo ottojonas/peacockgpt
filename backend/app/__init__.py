f749f17b7800fa3879d6344161b47ab9661f36e0
ottojonas <ottobjonas@outlook.com>
2025-02-11 09:43:45 +0000

    updated init and mongo

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,7 +14,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config["MONGO_URI"] = os.getenv("MONGODB_URI")
-    mongo.init_app()
+    print("MONGO_URI:", app.config["MONGO_URI"])
+    mongo.init_app(app)
     from .routes import routes
 
     app.register_blueprint(routes, url_prefix="/api")
