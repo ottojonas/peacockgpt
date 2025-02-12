f832c7c9fb60b559000c9d25f2c087ea73cf010b
ottojonas <ottobjonas@outlook.com>
2025-02-12 16:35:55 +0000

    updated sendMessage funciton

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -32,11 +32,13 @@ export default function Home() {
     if (!isAuthenticated) {
       router.push("/login");
     } else {
-      axios.get("/api/conversations", {
-	      params: { user_id: userId }
-      }).then((response) => {
-        setConversations(response.data); 
-      })
+      axios
+        .get("/api/conversations", {
+          params: { user_id: userId },
+        })
+        .then((response) => {
+          setConversations(response.data);
+        });
     }
   }, [isAuthenticated, router]);
 
@@ -85,20 +87,32 @@ export default function Home() {
         setConversations={setConversations}
         setMessages={setMessages}
         onDeleteConversation={handleDeleteConversation}
-	userId={userId}
+        userId={userId}
       />
       <Chat
         messages={messages}
         setMessages={setMessages}
         conversationKey={conversationKey}
         sendMessage={(text) =>
-          sendMessage(text, conversationKey, setMessages, setConversations)
+          sendMessage(
+            text,
+            conversationKey,
+            setMessages,
+            setConversations,
+            userId
+          )
         }
       />
       <ChatInput
         setMessages={setMessages}
         sendMessage={(text) =>
-          sendMessage(text, conversationKey, setMessages, setConversations)
+          sendMessage(
+            text,
+            conversationKey,
+            setMessages,
+            setConversations,
+            userId
+          )
         }
         inputValue={inputValue}
         setInputValue={setInputValue}
