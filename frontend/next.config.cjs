ab8f9197b24e5868d4e479b46b3f7edb41a72225
ottojonas <ottobjonas@outlook.com>
2025-02-17 16:37:26 +0000

    next.config.cjs added to root frontend

---
+++ frontend/next.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react",
+            "@babel/preset-typescript",
+          ],
+          plugins: ["@babel/plugin-transform-modules-commonjs"],
+        },
+      },
+    });
+  },
+  distDir: "build",
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/:path*", // proxy to flask backend hopefully
+      },
+    ];
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/static" : "",
+};
+
+module.exports = nextConfig;
