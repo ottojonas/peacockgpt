023e58333bcdb41ddcb529df3694448c9db4ca1b
ottojonas <ottobjonas@outlook.com>
2025-02-24 09:50:03 +0000

    DocumentHistory init

---
+++ frontend/components/DocumentHistory/DocumentHistory.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type DocumentItem = {
+  key: string;
+  title: string;
+  content: string;
+  isSelected: boolean;
+};
+
+interface Props {}
+
+const DocumentHistory = () => {
+  const fetchDocuments = async () => {};
+};
+
+export default DocumentHistory;
--- frontend/components/DocumentView/DocumentView.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const DocumentView = () => {
-  return;
-};
-export default DocumentView;
