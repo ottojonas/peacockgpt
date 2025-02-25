e524ae930f1e8c5c4cab0c31528d2be7c28e5ccf
ottojonas <ottobjonas@outlook.com>
2025-02-25 20:25:05 +0000

    init work for saving updated docs feat

--- frontend/components/DocumentInfo/DocumentInfo.tsx
+++ frontend/components/DocumentInfo/DocumentInfo.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DocumentTitle from "./DocumentTitle/DocumentTitle";
 import DocumentContent from "./DocumentContent/DocumentContent";
+import SaveIcon from "../icons/SaveIcon";
 
 export type DocumentInfoItem = {
   key: string;
@@ -28,12 +29,10 @@ const DocumentInfo: React.FC<DocumentInfoProps> = ({
 
   useEffect(() => {
     const fetchDocumentInfo = async (key: string) => {
-      console.log("Fetching document info for key: ", key);
       try {
         const response = await axios.get("/api/documents", {
           params: { key },
         });
-        console.log("Document info fetched: ", response.data);
         const document: DocumentInfoItem | undefined = response.data.find(
           (doc: DocumentInfoItem) => doc.key === key
         );
@@ -52,17 +51,35 @@ const DocumentInfo: React.FC<DocumentInfoProps> = ({
     }
   }, [documentKey, setTitle, setContent]);
 
+  const handleInformationSave = async () => {
+    try {
+      const response = await axios.put(`/api/documents?key=${documentKey}`);
+      const updatedDocumentInformation = response.data;
+    } catch (error) {
+      console.error("Error updating document information: ", error);
+    }
+  };
+
   return (
     <div
       className="documentContainer"
       style={{ marginLeft: "384px", marginRight: "320px" }}
     >
       <div className="px-4 pt-16 pb-48 mx-auto max-w-3x1 document-title">
-        <DocumentTitle title={documentTitle} setTitle = {setDocumentTitle} />
+        <DocumentTitle title={documentTitle} setTitle={setDocumentTitle} />
       </div>
       <div className="px-4 pt-16 pb-48 mx-auto max-w-3x1 document-content">
-        <DocumentContent content={documentContent} setContent={setDocumentContent}/>
+        <DocumentContent
+          content={documentContent}
+          setContent={setDocumentContent}
+        />
       </div>
+      <button
+        className="grid rounded-md w-9 h-9 place-items-center bg-card"
+        onClick={handleInformationSave}
+      >
+        <SaveIcon className="w-5 h-5" />
+      </button>
     </div>
   );
 };
