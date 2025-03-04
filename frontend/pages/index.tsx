e38d56da406c39ba0cfd09f09b3abfbb65e7898c
ottojonas <ottobjonas@outlook.com>
2025-03-04 10:58:42 +0000

    backend url set in index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ import io from "socket.io-client";
 import Info from "../components/Info";
 
 // * initialise socket connection
-const socket = io("http://localhost:5000", {
+const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
   transports: ["websocket"],
   withCredentials: true,
   reconnection: true,
