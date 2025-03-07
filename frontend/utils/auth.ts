490beb5f899bd3f4d5927632486813f103b4354c
ottojonas <ottobjonas@outlook.com>
2025-03-07 14:06:17 +0000

    added error and _log in utils

--- frontend/utils/auth.ts
+++ frontend/utils/auth.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { error } from "console";
 
 export const fetchAuthProviders = async () => {
   try {
@@ -21,6 +22,28 @@ export const fetchAuthSession = async () => {
     return response.data;
   } catch (error) {
     console.error("Error fetching auth session: ", error);
+    throw error;
+  }
+};
+
+export const fetchAuthError = async () => {
+  try {
+    const response = await axios.get("/api/auth/error", {
+      params: {error}
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+};
+
+export const authLog = async (message: string) => {
+  try {
+    const response = await axios.post("/api/auth/_log", {message});
+    return response.data;
+  } catch (error) {
+    console.error("Error sending log: ", error);
     throw error;
   }
 };
